45c2df17cf45f4033698e3c66fb29f5762565019
Michael Dondrup <mdondrup@gmail.com>
2026-04-08 19:49:55 +0200

    Added feature tests with Pest, updated dependencies. Database is seeded from a subset of the BilayerData.

--- tests/Feature/WelcomeTest.php
+++ tests/Feature/WelcomeTest.php
@@ -1,16 +1,25 @@
 <?php
 
-namespace Tests\Feature;
+test('the application returns a successful response', function () {
+    $response = $this->get('/');
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
+    $response->assertStatus(200)->assertSee('FAIRMD Lipids Databank');
+});
 
-class WelcomeTest extends TestCase
-{
-    /** @test */
-    public function urlValida()
-    {
-        $response = $this->get(route('welcome'));
-        $response->assertStatus(200);
-    }
-}
+test('the database contains the correct number of trajectories', function () {
+    $this->assertDatabaseCount('trajectories', 4);
+});
+
+test('the application shows correct number of trajectories', function () {
+    $response = $this->get('/');
+
+    $response->assertStatus(200);
+
+    // Normalize NBSP + whitespace and assert with regex.
+    $content = html_entity_decode($response->getContent(), ENT_QUOTES | ENT_HTML5, 'UTF-8');
+    $content = str_replace("\xC2\xA0", ' ', $content);
+    $content = preg_replace('/\s+/u', ' ', $content);
+    // get number of trajectories from the database and assert it is shown correctly
+    $trajectoriesCount = DB::table('trajectories')->count();
+    expect((bool) preg_match('/Total\s+trajectories\s*:\s*(?:<br\s*\/?>\s*)?' . $trajectoriesCount . '/i', $content))->toBeTrue();
+});
